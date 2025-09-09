2175285d97dee4da79e474f57d5080bbefe6c351
Raju Barman <rajubarman7665@gmail.com>
2025-09-10 00:31:36 +0530

    final commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -59,6 +59,50 @@ router.get('/user', async (req, res) => {
 
 })
 
+//login api
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body
 
+    // checking user already exists or not
+    const user = await userModel.findOne({
+        username
+    })
+    if (!user) {
+        return res.status(404).json({
+            message: "user account not found"
+        })
+    }
+    // checking password
+    const isPasswordValid = user.password === password
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            message: "Invalid password"
+        })
+    }
+    // creating jsonwebtoken for authentication
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    // send token in cookie
+    res.cookie('token', token,{
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 day
+    })
+    res.status(200).send({
+        message: "User logged in successfully",
+        user
+    })
+
+    
+
+
+
+})
+
+//logout api
+router.post('/logout', (req, res) => {
+    res.clearCookie('token')
+    
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+})
 
 module.exports = router;
